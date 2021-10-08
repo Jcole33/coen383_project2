bdbbfb64a287a0d61a6b2383dae84e8e7b62a647
Justin Cole <jcole@scu.edu>
2021-10-08 15:21:12 -0700

    set seed for random

--- src/Process.java
+++ src/Process.java
@@ -16,7 +16,7 @@ public class Process {
         this.globalClock = globalClock;
     }
     Process(Clock globalClock) {
-        Random rand = new Random();
+        Random rand = new Random(0);
         this.arrivalTime = (float) rand.nextInt(100); // will return numbetween 0 and 99
         this.expectedRuntime = (float) rand.nextInt(10) + 1; //will return num between 1 and 10
         this.priority = rand.nextInt(4) + 1;  //will return num between 1 and 4
