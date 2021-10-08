ea20b74a8ad044a56c2e78440513b8fc9fb1ef1f
Justin Cole <jcole@scu.edu>
2021-10-08 16:44:56 -0700

    added getters for process properties

--- src/main/Process.java
+++ src/main/Process.java
@@ -9,31 +9,46 @@ public class Process {
     float timeLeft;
     Clock globalClock;
 
-    Process(Clock globalClock, float arrivalTime, int priority, float expectedRuntime) {
+    public Process(Clock globalClock, float arrivalTime, int priority, float expectedRuntime) {
         this.arrivalTime = arrivalTime;
         this.priority = priority;
         this.expectedRuntime = expectedRuntime;
         this.timeLeft = expectedRuntime;
         this.globalClock = globalClock;
     }
-    Process(Clock globalClock) {
+    public Process(Clock globalClock) {
         Random rand = new Random(0);
-        this.arrivalTime = (float) rand.nextInt(100); // will return numbetween 0 and 99
-        this.expectedRuntime = (float) rand.nextInt(10) + 1; //will return num between 1 and 10
-        this.priority = rand.nextInt(4) + 1;  //will return num between 1 and 4
-        this.timeLeft = this.expectedRuntime;
+        arrivalTime = (float) rand.nextInt(100); // will return numbetween 0 and 99
+        expectedRuntime = (float) rand.nextInt(10) + 1; //will return num between 1 and 10
+        priority = rand.nextInt(4) + 1;  //will return num between 1 and 4
+        timeLeft = expectedRuntime;
         this.globalClock = globalClock;
     }
     
-    boolean run(float time) {
-        if (time < this.timeLeft) {
-            this.timeLeft -= time;
+    public boolean run(float time) {
+        if (time < timeLeft) {
+            timeLeft -= time;
         } else {
-            this.endTime = this.globalClock.time + this.timeLeft;
-            this.timeLeft = 0;
+            endTime = globalClock.time + timeLeft;
+            timeLeft = 0;
         }
-        this.globalClock.time += time;
-        return this.timeLeft == 0;
+        globalClock.time += time;
+        return timeLeft == 0;
+    }
+    public float getArrivalTime() {
+        return arrivalTime;
+    }
+    public int getPriority() {
+        return priority;
+    }
+    public float getTimeLeft() {
+        return timeLeft;
+    }
+    public float getExpectedRuntime() {
+        return expectedRuntime;
+    }
+    public float getEndTime() {
+        return endTime;
     }
 
 }
