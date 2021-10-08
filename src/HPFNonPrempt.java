8768f8c17de797eea62eb12563b4845d572eef4b
Justin Cole <jcole@scu.edu>
2021-10-08 14:55:05 -0700

    adding my algorithm class

---
+++ src/HPFNonPrempt.java
@@ -0,0 +1,17 @@
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class HPFNonPrempt {
+    Clock globalClock = new Clock();
+    Queue<Process> queue1 = new LinkedList<>();
+    Queue<Process> queue2 = new LinkedList<>();
+    Queue<Process> queue3 = new LinkedList<>();
+    Queue<Process> queue4 = new LinkedList<>();
+
+    HPFNonPrempt(int processCount) {
+
+        for (int i = 0; i < processCount; ++i) {
+
+        }
+    }
+}
