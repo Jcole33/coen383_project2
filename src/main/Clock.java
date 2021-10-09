1cb7fab2ee54488940e8ca6e0dbd8c8dca1401af
Justin Cole <jcole@scu.edu>
2021-10-08 18:02:51 -0700

    adding getters and setters for time

--- src/main/Clock.java
+++ src/main/Clock.java
@@ -1,4 +1,10 @@
 package main;
 public class Clock {
     float time = 0;
+    public float getTime() {
+        return time;
+    }
+    public void setTime(float time) {
+        this.time = time;
+    }
 }
