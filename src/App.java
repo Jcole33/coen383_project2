06193da6bc82c6b1b66bff619104f68b9cdcacca
Justin Cole <jcole@scu.edu>
2021-10-10 21:15:41 -0700

    removed individual code from app

--- src/App.java
+++ src/App.java
@@ -4,19 +4,7 @@ import HPF.HPF;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        HPF process = new HPFNonPrempt(4, false);
-        process.printProcesses();
-        System.out.println("HPFNonPrempt:");
-        process.run(100);
-        process = new HPFNonPrempt(4, true);
-        System.out.println("HPFNonPrempt with aging:");
-        process.run(100);
-        process = new HPFPrempt(4, false);
-        System.out.println("HPFPrempt:");
-        process.run(100);
-        process = new HPFPrempt(4, true);
-        System.out.println("HPFPrempt with aging:");
-        process.run(100);
+
     }
     
 }
