ebab1f3a27bbb1be42589bdfacee66b968ffb842
Justin Cole <jcole@scu.edu>
2021-10-10 20:15:52 -0700

    cleaned main function

--- src/App.java
+++ src/App.java
@@ -1,15 +1,7 @@
-import HPF.HPFNonPrempt;
-import HPF.HPF;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        HPF process = new HPFNonPrempt(4, false);
-        process.printProcesses();
-        process.run(100);
-        process.printStats();
-        process = new HPFNonPrempt(4, true);
-        process.run(100);
-        process.printStats();
+
     }
     
 }
